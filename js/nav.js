2f3043c027d741d09069b3dd6e28e6f61607d798
Jeremy Ansel <jeremyansel@hotmail.com>
2021-03-02 18:21:07 +0200

    Added expandable navbar & improved gallery design

---
+++ js/nav.js
@@ -0,0 +1,12 @@
+function toggleNav() {
+    let links = document.getElementsByClassName("links")[0];
+    let display = links.style.display;
+    console.log(display);
+    if(display == "block"){
+        links.style.display = "none";
+    }
+
+    else {
+        links.style.display = "block";
+    }
+}
