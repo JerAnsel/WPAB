e5c628e35770e8814fa7410458c4470b4b0d4784
Jeremy Ansel <jeremyansel@hotmail.com>
2021-05-10 13:01:27 +0300

    Added orientation classes, map and info

--- js/showcase-load.js
+++ js/showcase-load.js
@@ -2,16 +2,25 @@ function showcase_load(){
     let artist = sessionStorage.getItem('artist');
     let src = sessionStorage.getItem('src');
     let title = sessionStorage.getItem('title');
+    let age = sessionStorage.getItem('age')
     let location = sessionStorage.getItem('location');
     let gallery_type = sessionStorage.getItem('gallery-type'); 
     
     let showcase_div = document.getElementsByClassName('showcase')[0];
     let img = showcase_div.getElementsByTagName('img')[0];
     img.src = src;
+    let split_src = src.split('-')
+    if (split_src[split_src.length - 1].split('.')[0] == '%20v'){
+        img.classList.add('showcase-img-v')
+    }
+
+    else {
+        img.classList.add('showcase-img-h')
+    }
     let artist_elem = showcase_div.getElementsByClassName('artist')[0];
     artist_elem.innerHTML = artist;
-    title = document.createTextNode(title);
-    artist_elem.parentElement.appendChild(title);
-    console.log(location);
-    
+    let age_elem = showcase_div.getElementsByClassName('age')[0];
+    age_elem.innerHTML = age;
+    let title_elem = showcase_div.getElementsByClassName('title')[0];
+    title_elem.innerHTML = title;
 }
--- js/showcase-store.js
+++ js/showcase-store.js
@@ -2,7 +2,8 @@ function storeData(elem) {
     data = {};
     sessionStorage.setItem('artist', elem.getElementsByClassName('artist')[0].innerText);
     sessionStorage.setItem('src', elem.getElementsByTagName('img')[0]['src']);
-    sessionStorage.setItem('title', elem.getElementsByClassName('info')[0].textContent.split('.')[1]);
+    sessionStorage.setItem('title', elem.getElementsByClassName('info')[0].getElementsByClassName('title')[0].innerText);
+    sessionStorage.setItem('age', elem.getElementsByClassName('info')[0].getElementsByClassName('age')[0].innerText);
     sessionStorage.setItem('location', document.getElementsByClassName('location')[0].textContent); // Make sure this is useable
     sessionStorage.setItem('gallery-type', document.getElementsByClassName('location')[0].getElementsByClassName('loc-item')[0].textContent);
     
