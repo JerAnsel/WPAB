4ea1a6fc0e4e338ea95890bb44a15fe39835c587
Jeremy Ansel <jeremyansel@hotmail.com>
2021-05-11 12:13:41 +0300

    Created pen gallery, showcase with load and store

--- js/showcase-load.js
+++ js/showcase-load.js
@@ -1,4 +1,4 @@
-function showcase_load(){
+function showcase_load_image(){
     let artist = sessionStorage.getItem('artist');
     let src = sessionStorage.getItem('src');
     let title = sessionStorage.getItem('title');
@@ -17,6 +17,57 @@ function showcase_load(){
     else {
         img.classList.add('showcase-img-h')
     }
+    let artist_elem = showcase_div.getElementsByClassName('artist')[0];
+    artist_elem.innerHTML = artist;
+    let age_elem = showcase_div.getElementsByClassName('age')[0];
+    age_elem.innerHTML = age;
+    let title_elem = showcase_div.getElementsByClassName('title')[0];
+    title_elem.innerHTML = title;
+}
+
+function showcase_load_essay(){
+
+    let artist = sessionStorage.getItem('artist');
+    let src = sessionStorage.getItem('src');
+    console.log(src);
+
+    function requestEssay(src) {
+        var httpRequest;
+        httpRequest = new XMLHttpRequest();      
+        if (!httpRequest) {
+        alert('Giving up :( Cannot create an XMLHTTP instance');
+        return false;
+        }
+        httpRequest.onreadystatechange = function(){displayEssay(httpRequest);};
+        httpRequest.open('GET', 'https://www.aaciart.net/pen/' + src, true);
+		httpRequest.responseType = "document";
+        httpRequest.send();
+    }
+
+    function displayEssay(httpRequest){
+
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if (httpRequest.status === 200) {
+                let response = httpRequest.responseXML;
+				let content = response.getElementsByClassName('content')[0].innerHTML;
+                document.getElementsByClassName('content')[0].innerHTML = content;
+                        
+            
+            } 
+            else {
+                alert('There was a problem with the news request.');
+            }
+        }
+    }
+
+    requestEssay(src);
+
+    let title = sessionStorage.getItem('title');
+    let age = sessionStorage.getItem('age')
+    let location = sessionStorage.getItem('location');
+    
+    let showcase_div = document.getElementsByClassName('showcase')[0];
+
     let artist_elem = showcase_div.getElementsByClassName('artist')[0];
     artist_elem.innerHTML = artist;
     let age_elem = showcase_div.getElementsByClassName('age')[0];
--- js/showcase-store.js
+++ js/showcase-store.js
@@ -1,7 +1,19 @@
-function storeData(elem) {
+function storeImageData(elem) {
     data = {};
     sessionStorage.setItem('artist', elem.getElementsByClassName('artist')[0].innerText);
     sessionStorage.setItem('src', elem.getElementsByTagName('img')[0]['src']);
+    sessionStorage.setItem('title', elem.getElementsByClassName('info')[0].getElementsByClassName('title')[0].innerText);
+    sessionStorage.setItem('age', elem.getElementsByClassName('info')[0].getElementsByClassName('age')[0].innerText);
+    sessionStorage.setItem('location', document.getElementsByClassName('location')[0].textContent); // Make sure this is useable
+    sessionStorage.setItem('gallery-type', document.getElementsByClassName('location')[0].getElementsByClassName('loc-item')[0].textContent);
+    
+}
+
+function storeEssayData(elem) {
+    data = {};
+    let src = elem.getElementsByTagName('object')[0].getElementsByTagName('param')[0].getAttribute('value');
+    sessionStorage.setItem('artist', elem.getElementsByClassName('artist')[0].innerText);
+    sessionStorage.setItem('src', src);
     sessionStorage.setItem('title', elem.getElementsByClassName('info')[0].getElementsByClassName('title')[0].innerText);
     sessionStorage.setItem('age', elem.getElementsByClassName('info')[0].getElementsByClassName('age')[0].innerText);
     sessionStorage.setItem('location', document.getElementsByClassName('location')[0].textContent); // Make sure this is useable
